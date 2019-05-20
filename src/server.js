0bd480e397d5154ad764ee65998be8978b61e013
TimaAtim <32203323+TimaAtim@users.noreply.github.com>
2019-05-20 23:13:36 +0600

    'Create-quest' (#18)

--- src/server.js
+++ src/server.js
@@ -14,18 +14,29 @@ app.get("/", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", urlencodedParser, function (request, response) {
+app.get("/create-quest", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/create.html");
+});
+
+app.post("/create-quest", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
     myQuests.push(request.body);
-    
+
     // Возвращаем пользователя обратно на страницу, с которой пришли.
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/create.html");
 });
 
 // Url, отвечающий за выдачу массива созданных квестов
 app.get("/my-quests", urlencodedParser, function (request, response) {
-    response.send(myQuests);
+    if (myQuests.length > 0) {
+        var myQuestName = [];
+        myQuests.forEach(function (element) { myQuestName.push(element.quest_name) });
+        response.send(myQuestName);
+    }
+    else
+        response.send('У вас нет созданных квестов');
 });
+
 
 app.listen(3000);
